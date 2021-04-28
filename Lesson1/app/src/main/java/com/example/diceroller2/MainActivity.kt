e16484d25fec091591802071c327800ff5261981
Đại Nguyễn Đức Quốc <namedai01@outlook.com>
2021-04-28 16:49:59 +0700

    All file

---
+++ Lesson1/app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener {
+            rollDice()
+//            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun rollDice() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        
+        val randomInt = java.util.Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+//        resultText.text = randomInt.toString()
+    }
+}
